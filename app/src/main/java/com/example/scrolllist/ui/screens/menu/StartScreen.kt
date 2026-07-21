554cfa5815002e4990e82dd9936175c45bdfd695
0-A-0 <145715088+0-A-0@users.noreply.github.com>
2026-07-22 02:09:42 +0400

    Redesign StartScreen

--- app/src/main/java/com/example/scrolllist/ui/screens/menu/StartScreen.kt
+++ app/src/main/java/com/example/scrolllist/ui/screens/menu/StartScreen.kt
@@ -1,10 +1,17 @@
 package com.example.scrolllist.ui.screens.menu
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,11 +21,34 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.scrolllist.R
 import com.example.scrolllist.ui.theme.MyButton
+import com.example.scrolllist.ui.theme.burning
 import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 @Composable
 fun StartScreen(
@@ -26,40 +56,162 @@ fun StartScreen(
     StartInfinityGame: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-//    Image(
-//        modifier = Modifier.fillMaxSize(),
-//        painter = painterResource(R.drawable.main_back),
-//        contentDescription = null,
-//        contentScale = ContentScale.Crop
-//    )
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier,
+        modifier = modifier
+            .drawWithContent {
+                drawRect(
+//                color = Color(0xFF3B0B0B),
+//                color = Color(0xFF5A0D0D),
+//                Color(0xFF6B1124)
+                    color = Color(0xFFD5DCD2),
+                    topLeft = Offset(0f, 0f)
+                )
+                drawContent()
+            },
     ) {
-        Spacer(Modifier.fillMaxHeight(0.4f))
-        MyButton(
-            onClick = StartGame,
+
+        Spacer(Modifier.weight(1f))
+        Text(
+            text = "ShaitanBuster",
+            style = TextStyle(
+                fontSize = 44.sp,
+                fontWeight = FontWeight.Black,
+                fontFamily = FontFamily(Font(R.font.kelly_slab)),
+                color = Color.White,
+            ),
+            modifier = Modifier.burning()
+        )
+        Spacer(Modifier.weight(1f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .weight(3f)
+                .fillMaxWidth()
+                .graphicsLayer()
+                .drawWithCache {
+                    val widthRect = size.width * 0.08f
+                    val heightRect = size.height
+                    val x = (size.width - widthRect) / 2f
+                    val y = 0f
+                    val grassStep = size.width * 0.15f
+                    val path = Path().apply {
+                        moveTo(0f, size.height)
+                        quadraticTo(
+                            size.width * 0.05f,
+                            size.height * 0.9f,
+                            size.width * 0.2f,
+                            size.height * 0.8f
+                        )
+                        quadraticTo(size.width * 0.15f, size.height * 0.9f, grassStep, size.height)
+                        close()
+                    }
+                    val colorsGrass = listOf(
+                        Color(0xFF2B4B06),
+                        Color(0xFF147A18),
+                        Color(0xFF8BB956),
+                        Color(0xFF7FBD37),
+                        Color(0xFF7FBD37),
+                    )
+                    onDrawWithContent {
+                        drawRect(
+                            color = Color(0xFF000000),
+                            size = Size(widthRect, heightRect),
+                            topLeft = Offset(x, y)
+                        )
+//                        drawImage(
+//                            image = hat,
+//                            topLeft = Offset((size.width - hat.width)/2f,-size.height*0.05f)
+//                        )
+                        drawPath(
+                            path = path,
+                            color = Color(0xFF7FBD37)
+                        )
+                        var currentFill = 0f
+                        while (currentFill < size.width) {
+                            withTransform({
+                                translate(left = currentFill, top = 0f)
+                                scale(
+                                    0.7f + Random.nextFloat() * 0.6f,
+                                    0.7f + Random.nextFloat() * 0.6f,
+                                    Offset(0f, size.height)
+                                )
+                            }) {
+                                drawPath(
+                                    path = path,
+                                    color = colorsGrass.random()
+                                )
+                            }
+                            currentFill += grassStep
+                        }
+                        currentFill = grassStep * 0.8f
+                        while (currentFill < size.width) {
+                            val reflectBoolean = Random.nextBoolean()
+                            withTransform({
+                                translate(left = currentFill, top = 0f)
+                                if (reflectBoolean) scale(
+                                    -(0.7f + Random.nextFloat() * 0.6f),
+                                    0.7f + Random.nextFloat() * 0.6f,
+                                    Offset(0f, size.height)
+                                )
+                            }) {
+                                drawPath(
+                                    path = path,
+                                    color = colorsGrass.random()
+                                )
+                            }
+                            currentFill += grassStep
+                        }
+                        drawContent()
+                    }
+                },
         ) {
-            Text(
-                "Начать игру",
-                fontSize = 20.sp,
+            Spacer(Modifier.fillMaxSize(0.1f))
+            MyButton(
+                onClick = StartGame,
+                //            modifier = Modifier.weight(0.5f)
+            ) {
+                Text(
+                    "Начать игру",
+                    fontSize = 20.sp,
+                )
+            }
+            MyButton(
+                onClick = StartInfinityGame,
+                //            modifier = Modifier.weight(0.5f)
+            ) {
+                Text(
+                    "Бесконечный режим",
+                    fontSize = 20.sp
+                )
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .weight(2f)
+                .fillMaxWidth()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFF7FBD37),
+                            Color(0xFF2B4B06)
+                        ),
+                        endY = 100f
+                    )
+                )
+        ) {
+            AnimatedHelpText(
+                modifier = Modifier.heightIn(min = 56.dp)
             )
         }
-        MyButton(
-            onClick = StartInfinityGame,
-        ) {
-            Text(
-                "Бесконечный режим",
-                fontSize = 20.sp
-            )
-        }
-        Spacer(Modifier.fillMaxHeight(0.4f))
-        AnimatedHelpText()
     }
 }
 
 @Composable
-fun AnimatedHelpText() {
+fun AnimatedHelpText(modifier: Modifier = Modifier) {
     val alpha = remember { Animatable(0f) }
     val helpTexts = remember {
         listOf(
@@ -100,6 +252,15 @@ fun AnimatedHelpText() {
         text = helpText,
         textAlign = TextAlign.Center,
         fontSize = 14.sp,
-        modifier = Modifier.graphicsLayer { this.alpha = alpha.value }
+        modifier = modifier.graphicsLayer { this.alpha = alpha.value }
     )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF00ffff
+)
+@Composable
+fun StartScreenPreview() {
+    StartScreen(StartGame = {}, StartInfinityGame = {}, modifier = Modifier.fillMaxSize())
 }
--- app/src/main/java/com/example/scrolllist/ui/theme/Modifiers.kt
+++ app/src/main/java/com/example/scrolllist/ui/theme/Modifiers.kt
@@ -1,8 +1,11 @@
 package com.example.scrolllist.ui.theme
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.remember
@@ -10,34 +13,43 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import kotlinx.coroutines.delay
 
 val LocalShakeTrigger = compositionLocalOf { 0 }
-fun Modifier.shaking(localTrigger:(() -> Int)? = null): Modifier = composed {
+fun Modifier.shaking(localTrigger: (() -> Int)? = null): Modifier = composed {
     val trigger = rememberUpdatedState(LocalShakeTrigger.current)
     val rotation = remember { Animatable(0f) }
     LaunchedEffect(Unit) {
-        snapshotFlow{
-            localTrigger?.invoke()?: trigger.value
-        }.collect{trigger->
-        if (trigger > 0) {
-            val target = (30..100).random().toFloat()
-            println(target)
-            rotation.snapTo(target)
-            rotation.animateTo(0f, spring(Spring.DampingRatioHighBouncy, Spring.StiffnessMedium))
-        }
+        snapshotFlow {
+            localTrigger?.invoke() ?: trigger.value
+        }.collect { trigger ->
+            if (trigger > 0) {
+                val target = (30..100).random().toFloat()
+                println(target)
+                rotation.snapTo(target)
+                rotation.animateTo(
+                    0f,
+                    spring(Spring.DampingRatioHighBouncy, Spring.StiffnessMedium)
+                )
             }
+        }
     }
-    this.graphicsLayer{
+    this.graphicsLayer {
         rotationY = rotation.value
         cameraDistance = 8f * density
     }
 }
+
 // 0 рекомпозиций
-fun Modifier.jiggle(trigger:() -> Int): Modifier = composed {
+fun Modifier.jiggle(trigger: () -> Int): Modifier = composed {
     val offset = remember { Animatable(0f) }
     LaunchedEffect(Unit) {
-        snapshotFlow(trigger).collect { trigger->
+        snapshotFlow(trigger).collect { trigger ->
             if (trigger > 0) {
                 val target = (10..30).random().toFloat()
                 println(target)
@@ -46,5 +58,37 @@ fun Modifier.jiggle(trigger:() -> Int): Modifier = composed {
             }
         }
     }
-    this.graphicsLayer{translationX = offset.value}
+    this.graphicsLayer { translationX = offset.value }
+}
+
+@Composable
+fun Modifier.burning(durationMillis: Int = 3000, delayDuration:Long = 0L): Modifier {
+    val radius = remember { Animatable(0.001f) }
+    LaunchedEffect(Unit) {
+        delay(delayDuration)
+        radius.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = durationMillis, easing = LinearEasing),
+        )
+    }
+    return this
+        .graphicsLayer(alpha = 0.99f)
+        .drawWithContent {
+            drawContent()
+            val burnGradient = Brush.radialGradient(
+                0.0f to Color.Gray,
+                0.1f to Color.Black,
+                0.80f to Color.White,
+                0.88f to Color(0xFFFFD700),
+                0.94f to Color(0xFFFF4500),
+                0.98f to Color(0xFF8B0000),
+                1.0f to Color.Transparent,
+                center = center,
+                radius = radius.value * size.width
+            )
+            drawRect(
+                brush = burnGradient,
+                blendMode = BlendMode.SrcIn
+            )
+        }
 }
